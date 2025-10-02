fdddc89ef0a53d159698fe864f420903182db43a
baestero <leonardobaestero@gmail.com>
2025-10-02 01:52:04 -0300

    inicial commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://unregrettably-unsinkable-noriko.ngrok-free.dev";
+export const API_URL = "http://unregrettably-unsinkable-noriko.ngrok-free.dev/";
 
 export const TOKEN_POST = (body) => {
   return {
