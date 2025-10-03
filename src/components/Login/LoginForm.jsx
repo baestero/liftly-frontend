9ed0fd08a95076fbdd94b3707f82dfe4da496245
baestero <leonardobaestero@gmail.com>
2025-10-03 08:46:47 -0300

    padrinizando username antes de enviar

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -20,7 +20,7 @@ const LoginForm = () => {
     e.preventDefault();
 
     if (username.validate() && password.validate()) {
-      userLogin(username.value, password.value);
+      userLogin(username.value.toLowerCase(), password.value);
     }
   };
 
