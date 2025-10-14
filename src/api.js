b1495e59683672aa44160023997419b5c404a0b4
baestero <leonardobaestero@gmail.com>
2025-10-13 23:09:46 -0300

    inicial commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://liftly-backend-fjhi.onrender.com";
 
 //"https://liftly-backend-fjhi.onrender.com"
 //"http://localhost:3000"
